8e44b9aabd7a9d2f6c8d4112c1fee75a24eb590c
lutfiian <130535525+lutfiian@users.noreply.github.com>
2023-08-30 20:19:04 +0700

    Add files via upload

---
+++ Calert/CalertApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalertApp.swift
+//  Calert
+//
+//  Created by Fina Handalita on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CalertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
